3d65045156149d4eb4392d794429eb9e0d8f6359
crandino <carlosmanbe@hotmail.com>
2016-10-11 10:38:29 +0200

    New class definition on Activity1

--- app/src/main/java/carlos/whatsapp_dmob/Activity1.java
+++ app/src/main/java/carlos/whatsapp_dmob/Activity1.java
@@ -1,13 +1,23 @@
 package carlos.whatsapp_dmob;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Activity1 extends AppCompatActivity {
+public class Activity1 extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
     }
+
+    public void onClick(View arg0)
+    {
+
+    }
 }
+
+
